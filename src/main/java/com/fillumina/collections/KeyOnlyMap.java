e5398c9700811f9da479c5db6fdcb58c8efa9112
Francesco Illuminati <fillumina@gmail.com>
2020-12-26 20:21:14 +0100

    refactoring

--- src/main/java/com/fillumina/collections/KeyOnlyMap.java
+++ src/main/java/com/fillumina/collections/KeyOnlyMap.java
@@ -8,7 +8,8 @@ import java.util.Objects;
 import java.util.Set;
 
 /**
- * A container for its key set, which is a very usable set implementation.
+ * A container for its {@link #keySet()}, 
+ * which is a very usable set implementation.
  * 
  * @author Francesco Illuminati <fillumina@gmail.com>
  */
