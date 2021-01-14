3fdadbd1199e3ce9371e9391b00ea9e749bc7dfd
Francesco Illuminati <fillumina@gmail.com>
2021-01-14 11:28:50 +0100

    refactoring tests

--- src/test/java/com/fillumina/collections/SortedPairMapTest.java
+++ src/test/java/com/fillumina/collections/SortedPairMapTest.java
@@ -149,9 +149,9 @@ public class SortedPairMapTest extends AbstractArrayMapTestHelper {
     @Test
     public void testBuilder() {
         SortedPairMap<Integer,String> map = SortedPairMap.<Integer,String>builder()
+            .put(3, "three")
             .put(1, "one")
             .put(2, "two")
-            .put(3, "three")
             .build();
         
         assertEquals(3, map.size());
